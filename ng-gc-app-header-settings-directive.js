30421027aa13f2ce0867f9c915069f229257cd73
Philip Harrison <philip@mailharrison.com>
2013-09-15 18:41:48 +0100

    Fix build

--- ng-gc-app-header-settings-directive.js
+++ ng-gc-app-header-settings-directive.js
@@ -3,7 +3,11 @@
  * (c) 2013-2013 GoCardless, Ltd.
  * https://github.com/gocardless-ng/ng-gc-app-header-settings-directive.git
  * License: MIT
- */'use strict';
+ */
+(function(){
+'use strict';
+
+'use strict';
 
 angular.module('gc.appHeaderSettings', [
   'gc.toggle',
@@ -24,7 +28,8 @@ angular.module('gc.appHeaderSettings', [
   }
 ]);
 
-angular.module('app-header-settings-template.html', []).run(function($templateCache) {
+angular.module('app-header-settings-template.html', []).run(['$templateCache', function($templateCache) {
   $templateCache.put('app-header-settings-template.html',
     '<span class="u-relative"><button class="btn btn--hollow" toggle="appHeaderSettingsIsVisible"><i class="btn-icon btn-icon--centered ss-settings"></i></button><popover show="appHeaderSettingsIsVisible" class="header-settings-dropdown u-text-h6 u-padding-Hm u-padding-Vxs"><div ng-transclude=""></div></popover></span>');
-});
+}]);
+})();
